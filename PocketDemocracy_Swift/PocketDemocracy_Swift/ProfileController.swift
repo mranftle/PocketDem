41911e2a9b4c4f56cfffc85bcfcc609f1b4dc833
Austin Prince <amprince@wustl.edu>
2017-11-19 12:30:32 -0600

    commiting profile view controllers

--- PocketDemocracy_Swift/PocketDemocracy_Swift/ProfileController.swift
+++ PocketDemocracy_Swift/PocketDemocracy_Swift/ProfileController.swift
@@ -8,90 +8,64 @@
 
 import UIKit
 
-class ProfileController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
+class ProfileController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBAction func createEvents(_ sender: Any) {
+    }
+    @IBAction func segmentedControllerSelect(_ sender: Any) {
+        switch segmentedController.selectedSegmentIndex {
+        case 0:
+            eventView.isHidden = false
+        default:
+            eventView.isHidden = true
+        }
+    }
+    @IBOutlet weak var segmentedController: UISegmentedControl!
     var newsArticles: [String] = []
     
     
+    @IBOutlet weak var eventView: UIView!
     
-    @IBAction func segmentClick(_ sender: Any) {
-        switch segmentedControl.selectedSegmentIndex {
-        case 0:
-            <#code#>
-        default:
-            <#code#>
-        }
-    }
+//    @IBAction func segmentClick(_ sender: Any) {
+    @IBOutlet weak var eventsTable: UITableView!
+    
    
     var user: User?
-    var sizingCell: TagCell?
+   
     
     @IBOutlet weak var profImage: UIImageView!
     @IBOutlet weak var createEvent: UIButton!
-    @IBOutlet weak var flowLayout: FlowLayout!
-    @IBOutlet weak var collectionView: UICollectionView!
+
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var eventsTable: UITableView!
+
     @IBOutlet weak var organizationLabel: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         eventsTable.delegate = self
         eventsTable.dataSource = self
         user = GlobalVars.currentUser
-        collectionView.delegate = self
-        collectionView.dataSource = self
         nameLabel.text = user?.username
         profImage.contentMode = .scaleAspectFit
         profImage.image = user?.profPic
-        print ("\(user?.interests)")
-        print ("\(user?.actions)")
+       
     
         organizationLabel.text = user?.organization
-//        let cellNib = UINib(nibName: "TagCell", bundle: nil)
-//        self.collectionView.register(cellNib, forCellWithReuseIdentifier: "TagCell")
-//        self.collectionView.backgroundColor = UIColor.clear
-//        self.sizingCell = (cellNib.instantiate(withOwner: nil, options: nil) as NSArray).firstObject as! TagCell?
-//
-//        self.flowLayout.sectionInset = UIEdgeInsetsMake(8.0, 8.0, 8.0, 8.0)
-
+        if user?.organization == "Unaffiliated" {
+            segmentedController.isHidden = true
+        }
        
-        
-//        nameLabel.text! =
-        
-        
-        
+       
         
         // Do any additional setup after loading the view.
     }
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        print ("\(String(describing: user?.interests.count))")
-//        return (user?.interests.count)!
-//
-//    }
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        self.configureCell(cell: self.sizingCell!, forIndexPath: indexPath as NSIndexPath)
-//        return self.sizingCell!.systemLayoutSizeFitting(UILayoutFittingCompressedSize)
-//    }
-//
-//
-//    func configureCell(cell: TagCell, forIndexPath indexPath: NSIndexPath) {
-//        var TAGS = user?.interests
-//        let tag = TAGS![indexPath.row]
-//        cell.tagName.text = tag
-//    }
-//
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TagCell", for: indexPath) as! TagCell
-//        self.configureCell(cell: cell, forIndexPath: indexPath as NSIndexPath)
-//        return cell
-//
-//
-//    }
+  
+
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (user?.actions.count)!
     }
@@ -105,15 +79,15 @@ class ProfileController: UIViewController, UICollectionViewDataSource, UICollect
     
     override func viewWillAppear(_ animated: Bool) {
         
-         print("user = \(user!.username)")
     }
     
     @IBAction func logoutPressed(_ sender: UIButton) {
         GlobalVars.currentUser = nil
+       
         let loginController = self.storyboard!.instantiateViewController(withIdentifier: "LoginControllerID")
-        let interestsController = self.storyboard!.instantiateViewController(withIdentifier:"InterestControllerID")
+
         self.present(loginController, animated: true, completion: nil)
-        self.present(interestsController, animated:true, completion:nil)
+
         
     }
     
