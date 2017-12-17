6694e12b321c45240c34316cb547842858875c08
Zach Polsky <zacharypolsky@wustl.edu>
2017-12-17 02:56:05 -0600

    Added labels for pagination

--- PocketDemocracy_Swift/PocketDemocracy_Swift/Welcome2ViewController.swift
+++ PocketDemocracy_Swift/PocketDemocracy_Swift/Welcome2ViewController.swift
@@ -11,12 +11,29 @@ import UIKit
 class Welcome2ViewController: UIViewController {
     
     @IBOutlet weak var dismissButton: UIButton!
+    @IBOutlet weak var circle1: UIView!
+    @IBOutlet weak var circle2: UIView!
+    @IBOutlet weak var circle3: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         dismissButton.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
+        circle1.layer.cornerRadius = circle1.frame.size.width/2
+        circle1.clipsToBounds = true
+        circle1.layer.borderColor = UIColor.black.cgColor
+        circle1.layer.borderWidth = 1.0
+        
+        circle2.layer.cornerRadius = circle2.frame.size.width/2
+        circle2.clipsToBounds = true
+        circle2.layer.borderColor = UIColor.black.cgColor
+        circle2.layer.borderWidth = 1.0
+        
+        circle3.layer.cornerRadius = circle3.frame.size.width/2
+        circle3.clipsToBounds = true
+        circle3.layer.borderColor = UIColor.white.cgColor
+        circle3.layer.borderWidth = 1.0
     }
     
     override func didReceiveMemoryWarning() {
